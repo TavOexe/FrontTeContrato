2370c1a22837a0a9632a382bc65ee80e4892ba76
Gustavo <u201914306@upc.edu.pe>
2021-09-30 12:11:42 -0500

    fix: navbar

--- src/components/navbar.vue
+++ src/components/navbar.vue
@@ -1,22 +1,17 @@
 <template>
-  <v-card
-      class=" overflow-hidden"
-      height="1000"
-
-  >
-    <v-app-bar color=#283593>
-      <v-app-bar-nav-icon color="white" @click="drawer = true"></v-app-bar-nav-icon>
-      <v-toolbar-title class="white--text" left>
-        TeContrato
-      </v-toolbar-title>
-      <v-icon  x-large
-               dark>mdi-account</v-icon>
-      <v-icon  x-large
-               dark>mdi-home</v-icon>
-      <v-icon  x-large
-               dark>mdi-notification</v-icon>
-    </v-app-bar>
-
+  <v-flex>
+  <v-app-bar color=#283593>
+    <v-app-bar-nav-icon color="white" @click="drawer = true"></v-app-bar-nav-icon>
+    <v-toolbar-title class="white--text" left>
+      TeContrato
+    </v-toolbar-title>
+    <v-icon  x-large
+             dark>mdi-account</v-icon>
+    <v-icon  x-large
+             dark>mdi-home</v-icon>
+    <v-icon  x-large
+             dark>mdi-notification</v-icon>
+  </v-app-bar>
     <v-navigation-drawer
         v-model="drawer"
         absolute
@@ -24,41 +19,35 @@
     >
       <v-list nav dense>
         <v-list-item-group v-model="group" active-class="black--text text--accent-4" color=#283593>
-          <v-list-item >
+          <v-list-item>
             <v-list-item-icon>
               <v-icon>mdi-home</v-icon>
             </v-list-item-icon>
-            <v-btn plain :to="{name: '#'} " >Home</v-btn>
+            <v-btn plain :to="{name: 'dashboard'} " >Home</v-btn>
           </v-list-item>
           <v-list-item >
             <v-list-item-icon>
               <v-icon>mdi-email</v-icon>
             </v-list-item-icon>
-            <v-btn plain :to="{name: '#'} " >Proyect</v-btn>
+            <v-btn plain :to="{name: 'proyect'} " >Proyect</v-btn>
           </v-list-item>
           <v-list-item >
             <v-list-item-icon>
               <v-icon>mdi-message-text</v-icon>
             </v-list-item-icon>
-            <v-btn plain :to="{name: '#'} " >Configuration</v-btn>
+            <v-btn plain :to="{name: 'configuration'} " >Configuration</v-btn>
           </v-list-item>
           <v-list-item >
             <v-list-item-icon>
               <v-icon>mdi-account</v-icon>
             </v-list-item-icon>
-            <v-btn plain :to="{name: '#'} " >Log out</v-btn>
+            <v-btn plain :to="{name: 'Home'} " >Log out</v-btn>
           </v-list-item>
         </v-list-item-group>
       </v-list>
     </v-navigation-drawer>
-    <v-layout column align-center>
-      <offers></offers>
-      <offers></offers>
-      <offers></offers>
-    </v-layout>
 
-
-  </v-card>
+  </v-flex>
 </template>
 
 <script>
--- src/router/index.js
+++ src/router/index.js
@@ -18,6 +18,22 @@ const routes = [
     // which is lazy-loaded when the route is visited.
     component: () => import('../views/dashboard.vue')
   },
+  {
+    path: '/proyect',
+    name: 'proyect',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import('../views/proyect.vue')
+  },
+  {
+    path: '/configuration',
+    name: 'configuration',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import('../views/configuration.vue')
+  },
 
 ]
 
